667c7c1c675622f3caf4ac80b5a4c183f4bccac3
myerscar1555 <myerscl@rose-hulman.edu>
2018-11-26 15:14:31 -0500

    DONE

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,16 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Carter Myers.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
-
+import rosegraphics as rg
+window = rg.TurtleWindow
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,3 +29,30 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+size = 100
+wohlfie = rg.SimpleTurtle()
+wohlfie.pen = rg.Pen('Hot Pink', 5)
+wohlfie.speed = 7
+
+michael = rg.SimpleTurtle()
+michael.pen = rg.Pen('Green',5)
+michael.speed = 2
+
+for k in range(11):
+    wohlfie.draw_circle(size)
+
+    wohlfie.pen_up()
+    wohlfie.right(90)
+    wohlfie.forward(10)
+
+    wohlfie.pen_down()
+    size = size - 5
+
+    michael.go_to(rg.Point(0,200))
+    michael.pen_up()
+    michael.right(45)
+    michael.forward(2**k)
+    michael.pen_down()
+
+window.tracer(100)
+
